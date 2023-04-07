18fbab7df89082d158b07b0a85a9f8c07d81c88d
Nicholas Smith <thesmithdynasty@gmail.com>
2023-04-07 17:13:41 -0500

    prestride: checkpoint

--- conv_prestride.py
+++ conv_prestride.py
@@ -26,67 +26,56 @@ pad = torch.torch.nn.functional.pad
 
 
 def run_tests():
-    if True:
+    if False:
         #test2((8, 8), (3, 3), 2, 3)
         #test2((8, 8), (3, 3), stride=2, padding=0)
         #test2((9, 9), (3, 3), stride=2, padding=0)
         #test2((8, 8), (3, 3), stride=2, padding=1)
         #test2((8, 8), (3, 3), stride=3, padding=0)
         #test2((8, 8), (3, 3), stride=3, padding=1)
+        #test2((8, 8), (3, 3), stride=3, padding=2)
         #test2((8, 8), (3, 3), stride=5, padding=1)
         #test2((8, 8), (4, 4), stride=2, padding=0)
         #test2((8, 8), (4, 4), stride=4, padding=2)
         #test2((8, 8), (4, 4), stride=4, padding=0)
         #test2((8, 8), (4, 4), stride=5, padding=2)
-        #test2((8, 8), (7, 7), stride=2, padding=3)
         #test2((8, 8), (3, 3), stride=3, padding=1)
         #test2((8, 8), (5, 5), stride=5, padding=1)
         #test2((8, 8), (5, 5), stride=3, padding=0)
+
         #test2((8, 8), (5, 5), stride=3, padding=1)
-        #test2((8, 8), (5, 5), stride=3, padding=2)
+        test2((8, 8), (5, 5), stride=3, padding=2)
+        #test2((9, 9), (5, 5), stride=3, padding=1)
+
         #test2((8, 8), (5, 5), stride=3, padding=4)
-        test2((8, 8), (5, 5), stride=3, padding=7)
+        #test2((8, 8), (5, 5), stride=3, padding=7)
         #test2((8, 8), (5, 5), stride=4, padding=1)
         #test2((8, 8), (6, 6), stride=2, padding=1)
         #test2((8, 8), (6, 6), stride=4, padding=1)
         #test2((8, 8), (7, 7), stride=3, padding=3)
+        #test2((8, 8), (7, 7), stride=2, padding=3)
+        #test2((8, 8), (7, 7), stride=4, padding=2)
         #test2((18, 18), (7, 7), stride=5, padding=3)
         #test2((8, 8), (3, 3), stride=4, padding=1)
-        #test2((8, 8), (3, 3), stride=4, padding=1, m_override=4)
+        #test2((8, 8), (3, 3), stride=4, padding=1)
         #test2((8, 8), (3, 3), stride=4, padding=1)
         return
     if True:
+        total = 0
         for o in range(8, 14):
             for k in range(3, 9):
                 for s in range(2, k+1):
-                    for p in range(0, 5):
-                        test2((o, o), (k, k), s, p)
-    if False:
-        cache = {}
-        for o in range(8, 8 + 1):
-            for k in range(3, 9):
-                for s in range(2, k + 1):
-                    for p in range(0, 8):
-                        key = (k, s, p)# % s == 0)
-                        #if p % s == 0:
-                        #    continue
-                        #r = (cache[key], cache[key]+1) if key in cache else (0, 8)
-                        r = (0, 8)
-                        for m in range(*r):
-                            try:
-                                test2((o, o), (k, k), s, p, m_override=m)
-                                print(f"({k}, {s}, {p}) -> {m}")
-                                break
-                            except:
-                                continue
-                        if key in cache:
-                            assert cache[key] == m, f"{cache[key]} != {m}"
-                        else:
-                            cache[key] = m
+                    for p in range(0, 9):
+                        total += 1
+                        try:
+                            test2((o, o), (k, k), s, p)
+                        except ValueError:
+                            test2((o, o), (k, k), s, p, add_one=1)
+        print("Total", total)
 
 
-def test2(original_shape, kernel_size, stride, padding, m_override=None):
-    print(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding})")
+def test2(original_shape, kernel_size, stride, padding, add_one=0):
+    #print(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding})")
     orig_out = calculate_conv2d_output_dimensions(original_shape[1], original_shape[0], kernel_size, stride, (padding, padding, padding, padding))
 
     in_channels = 1
@@ -95,71 +84,25 @@ def test2(original_shape, kernel_size, stride, padding, m_override=None):
     #
     # Setup + Golden
     #
-    #torch_activations = torch.randn((1, in_channels, original_shape[0], original_shape[1]))
     torch_activations = torch.arange(original_shape[0] * original_shape[1], dtype=torch.float).reshape(1, 1, original_shape[0], original_shape[1])
-    #torch_weights = torch.randn((out_channels, in_channels, kernel_size[0], kernel_size[1]))
     torch_weights = torch.arange(kernel_size[0] * kernel_size[1], dtype=torch.float).reshape((1, 1, *kernel_size)) + 1.0
-    if False:
-        tmp = torch_weights
-        torch_weights = torch.zeros((out_channels, in_channels, kernel_size[0], kernel_size[1]))
-        k3_s2_4 = [
-            (0, 0, 0, 0),
-            (0, 0, 0, 2),
-            (0, 0, 2, 0),
-            (0, 0, 2, 2),
-            ]
-        k3_s2_1 = [
-            (0, 0, 0, 2),
-            ]
-        k5 = [
-            (0, 0, 4, 4),
-            ]
-        k4 = [
-            (0, 0, 3, 3),
-            ]
-        for s in k5:
-            torch_weights[s] = tmp[s]
-
-    #print(torch_activations)
+    #if True:
+    #    torch_weights = torch.zeros((1, 1, *kernel_size))
+    #    torch_weights[:, :, 4, 4] = 1.0
 
     torch_output = torch.nn.functional.conv2d(torch_activations, torch_weights, stride=stride, padding=padding)
-
+    assert orig_out == (torch_output.shape[-2], torch_output.shape[-1])
 
     # Make activations a multiple of stride
     torch_activations = pad(torch_activations, (0, round_up_mod(torch_activations.shape[-2], stride), 0, round_up_mod(torch_activations.shape[-1], stride)))
 
     # Calculate reduced padding
-
     ps_in_shape = ((original_shape[0] + stride - 1) // stride, (original_shape[1] + stride - 1) // stride)
-
-    #y = (original_y + padding[2] + padding[3] - (kernel_size[0] - 1) - 1) // stride[0] + 1
-    #orig_out[0] = (ps_in_shape[0] + init_padding + tail_padding - (ps_kernel_size - 1) - 1) // 1 + 1
-    #orig_out[0] = ps_in_shape[0] + init_padding + tail_padding - (ps_kernel_size - 1)
-    #orig_out[0] + ps_kernel_size = ps_in_shape[0] + init_padding + tail_padding + 1
-    #ps_kernel_size = ps_in_shape[0] + init_padding + tail_padding + 1 - orig_out[0]
-    #ps_kernel_size >= ps_in_shape[0] + init_padding + 1 - orig_out[0]
-
-    #
-    # Weight striding
-    #
     init_padding = (padding + stride - 1) // stride
-    print("init_padding", init_padding)
+    tail_padding = padding // stride + add_one
+    orig_tail_padding = (padding // stride)
 
-
-    m = m_override if m_override is not None else (stride - (kernel_size[0] % stride))
-
-    inequality = ps_in_shape[0] + init_padding - orig_out[0] + 1
-    print("asdf", ((kernel_size[0] + m) // stride) , inequality)
-    if ((kernel_size[0] + m) // stride) <= inequality:
-        m += stride
-
-    print("m", m)
-    mx = m
-    my = m
-    weights_view = torch_weights
-    weights_view = pad(weights_view, (0, mx, 0, my))
-    weights_view = weights_view.roll((-padding, -padding), (-2, -1))
-    print("weights_view", weights_view)
+    tail_padding += int(((kernel_size[0] - padding) % stride == 1) and tail_padding < init_padding)
 
     def get_offsets(k, stride, padding):
         offsets = torch.zeros(k, dtype=torch.int)
@@ -168,9 +111,20 @@ def test2(original_shape, kernel_size, stride, padding, m_override=None):
         offsets *= offsets.sum()
         return offsets
 
+    kernel_round_up = stride - (kernel_size[0] % stride)
+    final_out_diff = ps_in_shape[0] + init_padding + tail_padding - orig_out[0] + 1
+    if ((kernel_size[0] + kernel_round_up) // stride) < final_out_diff:
+        kernel_round_up += stride
+
+    mx = kernel_round_up
+    my = kernel_round_up
+    weights_view = torch_weights
+    weights_view = pad(weights_view, (0, mx, 0, my))
+    weights_view = weights_view.roll((-padding, -padding), (-2, -1))
+    #print("asdf", weights_view)
+
     y_offsets = get_offsets(kernel_size[1], stride, padding)
     x_offsets = get_offsets(kernel_size[0], stride, padding)
-    print("asdf", y_offsets, x_offsets, x_offsets.sum())
 
     ps_activations = []
     ps_weights = []
@@ -184,94 +138,49 @@ def test2(original_shape, kernel_size, stride, padding, m_override=None):
 
             w = weights_view
             w = w[:, :, y::stride, x::stride]
-            print((y, x), w, init_padding)
             w = w.roll((init_padding, init_padding), (-2, -1))
-            #print((y, x), w.shape)
             ps_weights.append(w)
 
     ps_activations = torch.cat(ps_activations, dim=-3)
+    assert ps_in_shape == (ps_activations.shape[-2], ps_activations.shape[-1])
     ps_weights = torch.cat(ps_weights, dim=-3)
-    print(ps_weights)
-    #print(torch_weights.shape, ps_weights.shape)
+
     v0 = torch_weights.shape[-3] * torch_weights.shape[-2] * torch_weights.shape[-1]
     v1 = ps_weights.shape[-3] * ps_weights.shape[-2] * ps_weights.shape[-1]
-    #print("  asdf", torch_weights.shape, ps_weights.shape)
-    #if v1 > v0:
-    #    print("asdf", v0, v1, v1 / v0)
-
 
     #
     # Modified padding
     #
     ps_kernel_size = ps_weights.shape[-2]
-    for tail_padding in [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]:
-        ps_padding = (
-            init_padding,
-            tail_padding,
-            init_padding,
-            tail_padding,
+    tail_padding = orig_out[0] - ps_in_shape[0] - init_padding + (ps_kernel_size - 1)
+    ps_padding = (
+        init_padding,
+        tail_padding,
+        init_padding,
+        tail_padding,
         )
-        strided_out = calculate_conv2d_output_dimensions(ps_in_shape[1], ps_in_shape[0], ps_kernel_size, 1, ps_padding)
-        if strided_out == orig_out:
-            break
-    print("tail_padding", tail_padding)
 
     #
     # Verify
     #
     ps_activations = pad(ps_activations, ps_padding)
     #print(ps_padding)
-    print(ps_activations)
-    print(ps_weights)
+    #print(ps_activations)
+    #print(ps_weights)
     ps_output = torch.nn.functional.conv2d(ps_activations, ps_weights, stride=1, padding=0)
-    assert torch.allclose(torch_output, ps_output, atol=1e-04), f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding})\n{torch_output}\n{ps_output}\nFAILURE"
-    print(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding})\n{torch_output}\n{ps_output}\nSUCCESS")
-    return m
+    f = torch.allclose(torch_output, ps_output, atol=1e-04)
+    if not f:
+        assert ((kernel_size[0] - padding) % stride == 1) and tail_padding < init_padding
+        assert ps_weights[0, 0, 0, 0] == float(kernel_size[0] * kernel_size[1]), f"{padding} {init_padding}"
+        #print(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding}) {ps_in_shape} {(init_padding, tail_padding, orig_tail_padding)} {orig_out} {v1 / v0}")
+        raise ValueError(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding})\n{torch_output}\n{ps_output}\n{ps_weights}\n{tail_padding}\nFAILURE")
+    asdf = (((original_shape[0] + padding + padding) - (kernel_size[0])) % stride)
+    k = (kernel_size[0] + stride - 1) // stride
+    asdf = (((ps_in_shape[0] + init_padding + orig_tail_padding) - k))# % stride)
+    exact = (asdf <= 1)
+    #exact = ((asdf + stride - 1) // stride)
+    if add_one:
+        print(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding}) {ps_in_shape} {(init_padding, tail_padding, orig_tail_padding)} {orig_out} [{asdf}, {exact}] {v1 / v0}")
+    print(f"test2({original_shape}, {kernel_size}, stride={stride}, padding={padding}) {v1 / v0} {tuple(ps_weights.shape[2:])} {ps_weights[0, 0, 0, 0]}")
 
-
-def test(original_shape, kernel_size, stride, padding):
-    print(f"test({original_shape}, {kernel_size}, stride={stride}, padding={padding})")
-    pre_strided_in_shape = ((original_shape[0] + stride - 1) // stride, (original_shape[1] + stride - 1) // stride)
-    orig_out = calculate_conv2d_output_dimensions(original_shape[1], original_shape[0], kernel_size, stride, (padding, padding, padding, padding))
-
-    pad_up = 0 if (kernel_size[0] >= stride or padding == 0) else (stride - padding)
-    init_window = padding % kernel_size[0] != 0
-
-    init_padding = (padding + stride - 1) // stride
-    tail_padding = (round_up_mod((original_shape[0] + padding - kernel_size[0] + 1), stride) + padding) // stride + init_window
-
-    pre_strided_kernel_size = (kernel_size[0] + stride - 1) // stride + init_window + pad_up
-    pre_volume = pre_strided_kernel_size * pre_strided_kernel_size * stride * stride
-    assert pre_volume >= kernel_size[0] * kernel_size[1], f"({pre_strided_kernel_size}, {pre_strided_kernel_size}) {stride} v={pre_volume} {kernel_size[0] * kernel_size[1]}"
-
-    print("  asdf", (init_padding, tail_padding), pad_up, init_window)
-
-    pre_strided_padding = (padding + stride - 1) // stride
-    pre_strided_padding = (
-        init_padding,
-        tail_padding,
-        init_padding,
-        tail_padding,
-    )
-
-    strided_out = calculate_conv2d_output_dimensions(pre_strided_in_shape[1], pre_strided_in_shape[0], pre_strided_kernel_size, 1, pre_strided_padding)
-
-    print("  pre_strided_kernel_size", (pre_strided_kernel_size, pre_strided_kernel_size), "padding", pre_strided_padding, "orig", original_shape, "pre_in", pre_strided_in_shape, "out", orig_out)
-    assert orig_out == strided_out, f"{orig_out} != {strided_out}"
-    print("  PASSED" if orig_out == strided_out else "  FAILED")
-    return orig_out == strided_out
-
-
-if False:
-    num_fails = 0
-    for o in range(8, 16):
-        for k in range(2, 8):
-            for s in range(2, 5):
-                for p in range(0, 5):
-                    num_fails += not test((o, o), (k, k), s, p)
-    print("Num fails", num_fails)
-elif False:
-    test((8, 8), (4, 4), 2, 1)
-    test((8, 8), (4, 4), 2, 2)
-    test((10, 10), (4, 4), 2, 2)
 run_tests()
